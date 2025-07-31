6789ffa24474db3fa907f8dc2e2e6835cf18dfd8
Adam McKee <adam.be.g84d@gmail.com>
2025-07-31 10:14:54 -0500

    cursor request improved typing

--- packages/data/src/userData/navHistory.ts
+++ packages/data/src/userData/navHistory.ts
@@ -18,19 +18,21 @@ export async function readRecentNav(
 ): Promise<Array<RepositoryId>> {
     const db = await connectToDb()
     return new Promise((res, rej) => {
-        const limit = opts?.limit || LIMIT_NAV
         const tx = db.transaction([DB_STORE_NAV], 'readonly')
-        const repos: Array<RepositoryId> = []
-
-        tx
+        const req: IDBRequest<IDBCursorWithValue | null> = tx
             .objectStore(DB_STORE_NAV)
             .index(DB_INDEX_NAV)
-            .openCursor(null, 'prev').onsuccess = e => {
-            if (repos.length < limit) {
-                const cursor = (e.target as any).result
-                if (cursor) {
-                    const record = cursor.value as NavRecord
-                    repos.push(record.repo)
+            .openCursor(null, 'prev')
+
+        const limit = opts?.limit || LIMIT_NAV
+        const repos: Array<RepositoryId> = []
+
+        req.onsuccess = () => {
+            const cursor: IDBCursorWithValue | null = req.result
+            if (cursor) {
+                const record = cursor.value as NavRecord
+                repos.push(record.repo)
+                if (repos.length < limit) {
                     cursor.continue()
                 }
             }
