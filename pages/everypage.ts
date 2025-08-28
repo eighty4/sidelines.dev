699c7a7343c32a5ceec0a3c3dbe42d537ba9809f
Adam McKee <adam.be.g84d@gmail.com>
2025-08-27 21:41:47 -0500

    show indicator on esbuild updates and update css updates in browser

--- pages/everypage.ts
+++ pages/everypage.ts
@@ -17,19 +17,94 @@ if (sidelines.IS_DEV) {
             'change',
             (e: MessageEvent) => {
                 const change: EsbuildEvent = JSON.parse(e.data)
-                console.log('esbuild update', change)
-                const indicator = document.createElement('div')
-                indicator.style.border = '6px dashed orange'
-                indicator.style.zIndex = '9000'
-                indicator.style.position = 'fixed'
-                indicator.style.top = indicator.style.left = '1px'
-                indicator.style.height = indicator.style.width =
-                    'calc(100% - 2px)'
-                indicator.style.boxSizing = 'border-box'
-                document.body.appendChild(indicator)
+                const cssUpdates = change.updated.filter(p =>
+                    p.endsWith('.css'),
+                )
+                if (cssUpdates.length) {
+                    console.log('esbuild css updates', cssUpdates)
+                    const cssLinks: Record<string, HTMLLinkElement> = {}
+                    for (const elem of document.getElementsByTagName('link')) {
+                        if (elem.getAttribute('rel') === 'stylesheet') {
+                            const url = new URL(elem.href)
+                            if ((url.host = location.host)) {
+                                cssLinks[url.pathname] = elem
+                            }
+                        }
+                    }
+                    let swappedCss: boolean = false
+                    for (const cssUpdate of cssUpdates) {
+                        const cssLink = cssLinks[cssUpdate]
+                        if (cssLink) {
+                            const next = cssLink.cloneNode() as HTMLLinkElement
+                            next.href = `${cssUpdate}?${Math.random().toString(36).slice(2)}`
+                            next.onload = () => cssLink.remove()
+                            cssLink.parentNode!.insertBefore(
+                                next,
+                                cssLink.nextSibling,
+                            )
+                            swappedCss = true
+                        }
+                    }
+                    if (swappedCss) {
+                        addCssUpdateIndicator()
+                    }
+                }
+                if (cssUpdates.length < change.updated.length) {
+                    const jsUpdates = change.updated.filter(
+                        p => !p.endsWith('.css'),
+                    )
+                    const jsScripts: Set<string> = new Set()
+                    for (const elem of document.getElementsByTagName(
+                        'script',
+                    )) {
+                        const url = new URL(elem.src)
+                        if ((url.host = location.host)) {
+                            jsScripts.add(url.pathname)
+                        }
+                    }
+                    if (jsUpdates.some(jsUpdate => jsScripts.has(jsUpdate))) {
+                        console.log('esbuild js updates require reload')
+                        addJsReloadIndicator()
+                    }
+                }
             },
         )
     }
+}
+
+function addCssUpdateIndicator() {
+    const indicator = createUpdateIndicator('green', '9999')
+    indicator.style.transition = 'opacity ease-in-out .38s'
+    indicator.style.opacity = '0'
+    indicator.ontransitionend = () => {
+        if (indicator.style.opacity === '1') {
+            indicator.style.opacity = '0'
+        } else {
+            indicator.remove()
+            indicator.onload = null
+            indicator.ontransitionend = null
+        }
+    }
+    document.body.appendChild(indicator)
+    setTimeout(() => (indicator.style.opacity = '1'), 0)
+}
+
+function addJsReloadIndicator() {
+    document.body.appendChild(createUpdateIndicator('orange', '9000'))
+}
+
+function createUpdateIndicator(
+    color: 'green' | 'orange',
+    zIndex: '9000' | '9999',
+): HTMLDivElement {
+    const indicator = document.createElement('div')
+    indicator.style.border = '6px dashed ' + color
+    indicator.style.zIndex = zIndex
+    indicator.style.position = 'fixed'
+    indicator.style.top = indicator.style.left = '1px'
+    indicator.style.height = indicator.style.width = 'calc(100% - 2px)'
+    indicator.style.boxSizing = 'border-box'
+    return indicator
 }
 
 if (sidelines.IS_PROD) {
